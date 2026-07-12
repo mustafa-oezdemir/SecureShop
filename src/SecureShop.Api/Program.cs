d716563495dd8b0a35bc544f247857157acf8501
Mustafa Özdemir <mustafa.ozdemir1408@gmail.com>
2026-07-12 16:43:02 +0200

    app data contex

--- src/SecureShop.Api/Program.cs
+++ src/SecureShop.Api/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using SecureShop.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
 
 builder.Services.AddControllers();
 
